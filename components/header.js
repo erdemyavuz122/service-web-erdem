80ae20dc2378437d2932b10c341dfbef86f990fb
erdem yavuz <eerdemyavuz@gmail.com>
2025-07-10 14:31:27 +0300

    hizmetlerimiz kısmına dropdown eklendi

--- components/header.js
+++ components/header.js
@@ -1,34 +1,82 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [dropdown, setDropdown] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link className="navbar-brand" href="/">
+        <Link className="navbar-brand fw-bold" href="/">
           Beyaz Eşya Servis
         </Link>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-target="#mainNavbar"
+          aria-controls="mainNavbar"
           aria-expanded="false"
-          aria-label="Toggle navigation"
+          aria-label="Menüyü Aç/Kapat"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto">
+        <div className="collapse navbar-collapse" id="mainNavbar">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center">
             <li className="nav-item">
               <Link className="nav-link" href="/">
                 Ana sayfa
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="/hizmet">
+            {/* Sadece Hizmetlerimiz Dropdown */}
+            <li
+              className="nav-item dropdown"
+              onMouseEnter={() => setDropdown(true)}
+              onMouseLeave={() => setDropdown(false)}
+              style={{ position: "relative" }}
+            >
+              <Link
+                href="/hizmet"
+                className="nav-link dropdown-toggle"
+                aria-expanded={dropdown ? "true" : "false"}
+                onClick={() => setDropdown(false)}
+                id="hizmetlerimiz"
+                role="button"
+                tabIndex={0}
+              >
                 Hizmetlerimiz
               </Link>
+              <ul
+                className={`dropdown-menu${dropdown ? " show" : ""}`}
+                aria-labelledby="hizmetlerimiz"
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  top: "100%",
+                  zIndex: 1000,
+                  display: dropdown ? "block" : "none",
+                }}
+              >
+                {[
+                  "Beyaz Eşya Servisi",
+                  "Buzdolabı",
+                  "Çamaşır Makinesi",
+                  "Bulaşık Makinesi",
+                  "Fırın",
+                  "Klima",
+                  "Kombi",
+                  "Televizyon",
+                ].map((servis, i) => (
+                  <li key={i}>
+                    <Link
+                      className="dropdown-item"
+                      href={`/hizmet?service=${encodeURIComponent(servis)}`}
+                    >
+                      {servis}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </li>
             <li className="nav-item">
               <Link className="nav-link" href="/hakkimizda">
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import TelefonButton from "@/components/telefon-button";
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function MyApp({ Component, pageProps }) {
   return (
